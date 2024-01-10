27875a79da4bbde0057d0462296870f8f4c4ec83
Gary Gause <garygause@gmail.com>
2024-01-10 09:21:02 -0800

    add map

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
+declare var google: any;
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const API_KEY = '';
-const API_URL =
-  'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=';
+const API_KEY = 'AIzaSyAhIS4ZkB5arBMj48IoDYpad466A9xNCM4';
+const API_URL = 'https://maps.googleapis.com/maps/api/js?key=&callback=initMap';
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -20,6 +21,11 @@ function searchAddressHandler(event: Event) {
     )
     .then((response) => {
       const coords = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: coords,
+        zoom: 16,
+      });
+      new google.maps.Marker({ position: coords, map: map });
       console.log(coords);
     })
     .catch((error) => console.log(error));
